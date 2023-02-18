600bda44b6dd778c56fcb31e811f8f19d4b9d7c6
Thanh Hau Nguyen <t.nguyen@sprout.ai>
2023-02-18 16:22:23 +0000

    update generating data

--- data/generate_data.py
+++ data/generate_data.py
@@ -13,18 +13,19 @@ import pandas as pd
 from tqdm import tqdm
 
 
-login()
+# login()
+
 
 def generate(args):
     st_model = SentenceTransformer(args.sentence_transformers_weights_path)
 
     # Download the parquet table
     print("Downloading metadata.parquet...")
-    table_url = f'https://huggingface.co/datasets/poloclub/diffusiondb/resolve/main/metadata.parquet'
-    urlretrieve(table_url, 'metadata.parquet')
-    metadata_df = pd.read_parquet('metadata.parquet')
+    table_url = f"https://huggingface.co/datasets/poloclub/diffusiondb/resolve/main/metadata.parquet"
+    urlretrieve(table_url, "metadata.parquet")
+    metadata_df = pd.read_parquet("metadata.parquet")
 
-    prompts = list(metadata_df.prompt.values)[args.start: args.end]
+    prompts = list(metadata_df.prompt.values)[args.start : args.end]
 
     print("Encoding prompts...")
     embeddings = []
@@ -32,13 +33,11 @@ def generate(args):
         prompt_embeddings = st_model.encode(prompt).flatten()
         embeddings.append(prompt_embeddings)
 
-
     class CFG:
         device = "cuda" if torch.cuda.is_available() else "cpu"
         seed = 42
         generator = torch.Generator(device).manual_seed(seed)
         model_id = "stabilityai/stable-diffusion-2"
-
 
     cfg = CFG()
 
@@ -47,8 +46,9 @@ def generate(args):
     )
     model_pipe = model_pipe.to(cfg.device)
 
-
-    def generate_image(prompt, idx, model, n_images=1, image_folder=args.save_image_dir):
+    def generate_image(
+        prompt, idx, model, n_images=1, image_folder=args.save_image_dir
+    ):
         image_paths = []
         for i in range(n_images):
             image = model(prompt).images[0]
@@ -61,14 +61,14 @@ def generate(args):
 
         return image_paths
 
-
     generated_image_paths = []
     images_idx = []
     for i, prompt in enumerate(prompts):
         idx = i + args.start
-        generated_image_paths.append(generate_image(prompt, idx, model_pipe, args.n_images_per_prompt))
+        generated_image_paths.append(
+            generate_image(prompt, idx, model_pipe, args.n_images_per_prompt)
+        )
         images_idx.append(idx)
-
 
     prompt_ids = []
     image_ids = []
@@ -88,7 +88,6 @@ def generate(args):
 
         all_prompt_images.append(prompt_images)
 
-
     prompt_df = pd.DataFrame({"id": prompt_ids, "text": prompts, "emb": embeddings})
 
     image_df = pd.DataFrame({"id": image_ids, "path": image_paths})
@@ -100,17 +99,19 @@ def generate(args):
         }
     )
 
-
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
     correlation_df["fold"] = -1
     for fold, (train_idx, val_idx) in enumerate(kf.split(correlation_df)):
         correlation_df.loc[val_idx, "fold"] = fold
 
     # write to csv
-    prompt_df.to_csv(os.path.join(args.save_dir, f"prompt_{args.start}.csv"), index=False)
+    prompt_df.to_csv(
+        os.path.join(args.save_dir, f"prompt_{args.start}.csv"), index=False
+    )
     image_df.to_csv(os.path.join(args.save_dir, f"image_{args.start}.csv"), index=False)
-    correlation_df.to_csv(os.path.join(args.save_dir, f"correlation_{args.start}.csv"), index=False)
-
+    correlation_df.to_csv(
+        os.path.join(args.save_dir, f"correlation_{args.start}.csv"), index=False
+    )
 
     pairs = []
     for prompt_id in correlation_df.prompt_id.unique():
@@ -131,7 +132,9 @@ def generate(args):
             image_id for image_ids in negative_images for image_id in image_ids
         ]
 
-        pairs.extend([(prompt_id, random.choice(negative_images), 0) for _ in range(10)])
+        pairs.extend(
+            [(prompt_id, random.choice(negative_images), 0) for _ in range(10)]
+        )
 
     pairs_df = pd.DataFrame(pairs, columns=["prompt_id", "image_id", "target"])
     pairs_df.to_csv(os.path.join(args.save_dir, f"pairs_{args.start}.csv"), index=False)
@@ -143,7 +146,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--save_dir", type=str, default="./generated_data")
     parser.add_argument(
-        "--sentence_transformers_weights_path", type=str, default="/home/thanh/shared_disk/thanh/sditp/data/all-MiniLM-L6-v2"
+        "--sentence_transformers_weights_path",
+        type=str,
+        default="/home/thanh/shared_disk/thanh/sditp/data/all-MiniLM-L6-v2",
     )
     parser.add_argument("--start", type=int, default=0)
     parser.add_argument("--end", type=int, default=10)
@@ -160,4 +165,4 @@ if __name__ == "__main__":
 
     # download sentence transformers weights here: https://www.kaggle.com/datasets/inversion/sentence-transformers-222
     # sample usage
-    # python generate.py --save_dir ./generated_data --start 0 --end 3000 --n_images_per_prompt 4 --sentence_transformers_weights_path /home/thanh/shared_disk/thanh/sditp/data/all-MiniLM-L6-v2
+    # python generate_data.py --save_dir ./generated_data --start 0 --end 3000 --n_images_per_prompt 4 --sentence_transformers_weights_path /home/thanh/shared_disk/thanh/sditp/data/all-MiniLM-L6-v2
---
+++ data/start_generate.py
@@ -0,0 +1,29 @@
+# run generate_data.py with n processes, each with one GPU device, with multiprocessing Pool
+
+import os
+import multiprocessing
+import subprocess
+
+
+def run_generate_data(i, sentence_transformers_weights_path):
+    num_images_per_process = 5000
+    num_images_per_prompt = 4
+    subprocess.run(
+        f"CUDA_VISIBLE_DEVICES={i} python generate_data.py --start {i * num_images_per_process} --end {(i + 1) * num_images_per_process} --n_images_per_prompt {num_images_per_prompt} --sentence_transformers_weights_path {sentence_transformers_weights_path}",
+        shell=True,
+    )
+
+
+if __name__ == "__main__":
+    num_processes = 8
+    sentence_transformers_weights_path = (
+        "/home/thanh/shared_disk/thanh/sditp/data/all-MiniLM-L6-v2"
+    )
+    with multiprocessing.Pool(num_processes) as p:
+        p.starmap(
+            run_generate_data,
+            [(i, sentence_transformers_weights_path) for i in range(num_processes)],
+        )
+
+# huggingface-cli login
+# download sentence transformers weights here: https://www.kaggle.com/datasets/inversion/sentence-transformers-222
