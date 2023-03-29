014dc46990bab412b67e70732281c67a5a33720c
Thanh Hau Nguyen <thanhhau097@gmail.com>
2023-03-29 04:17:34 +0000

    update metadat-large

--- data/generate_data.py
+++ data/generate_data.py
@@ -22,7 +22,7 @@ def generate(args):
 
     # Download the parquet table
     print("Downloading metadata.parquet...")
-    table_url = f"https://huggingface.co/datasets/poloclub/diffusiondb/resolve/main/metadata.parquet"
+    table_url = f"https://huggingface.co/datasets/poloclub/diffusiondb/resolve/main/metadata-large.parquet"
     urlretrieve(table_url, "metadata.parquet")
     metadata_df = pd.read_parquet("metadata.parquet")
 
